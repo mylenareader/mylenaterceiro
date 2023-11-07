72f8c1a48a24f4dc6c17a76dde4f9a3b5715f988
mylenareader <130715968+mylenareader@users.noreply.github.com>
2023-11-07 20:07:10 -0300

    0711aula7ok

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ alert('Olá, Mundo!');
 function <!-- colocar o nome do som, no slide como TOCASOMPOM  --> (){
     document.querySelector <!--('#som_tecla_pom')-->. play();
 }
+document.querySelector <!--('.tecla_pom ')--> .onclick = tocaSomPom
+
+function <!-- colocar o nome do som, no slide como TOCASOMPOM  --> (){
+    document.querySelector <!--('#som_tecla_pom')-->. play();
+}
+document.querySelector <!--('.tecla_pom ')--> .onclick = tocaSomClap
+
+<!-- perguntar pro professor o que é console do navegador, slide 7 da aula 51. slide 7-->
+
+document.querySelectorAll('.tecla');  
